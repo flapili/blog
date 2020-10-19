1abc5999c5d2733b75ec8699ba483d89324c739f
Deveaux Benoît <contact.flapili@gmail.com>
2020-10-19 16:45:00 +0200

    add bio

---
+++ pages/bio.vue
@@ -0,0 +1,9 @@
+<template>
+    <el-row>
+      <el-col :md="{ span: 16, offset: 4 }">
+        <el-card shadow="hover">
+          <h1 class="text-center">Biographie</h1>
+        </el-card>
+      </el-col>
+    </el-row>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,24 @@
   <div>
     <el-row>
       <el-col :md="{ span: 16, offset: 4 }">
-        <el-card>Accueil</el-card>
+        <el-card shadow="hover">
+          <h1 class="text-center">Bienvenue sur mon site perso !</h1>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row style="margin-top: 50px">
+      <el-col :md="{ span: 4, offset: 5 }">
+        <nuxt-link to="/bio" class="no-text-decoration">
+          <el-card shadow="hover" class="text-center">Biographie</el-card>
+        </nuxt-link>
+      </el-col>
+      <el-col :md="{ span: 4, offset: 1 }">
+        <el-card shadow="hover" class="text-center"> Curriculum Vitae </el-card>
+      </el-col>
+      <el-col :md="{ span: 4, offset: 1 }">
+        <nuxt-link to="/posts" class="no-text-decoration">
+          <el-card shadow="hover" class="text-center">Posts</el-card>
+        </nuxt-link>
       </el-col>
     </el-row>
   </div>
