2ecc3fc54e6b01ffc022a89cb29f1c13d6df0dcb
Deveaux Benoît <contact.flapili@gmail.com>
2020-11-06 19:55:28 +0100

    add status code

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
-    <h1 v-if="error.statusCode === 404">Page non trouvée</h1>
+    <div v-if="error.statusCode === 404">
+     <h1 class="not-found-error-code">404</h1>
+     <h1 class="not-found-message">Page non trouvée</h1>
+    </div>
+
     <h1 v-else>Une erreur est survenue</h1>
     <div>
         <NuxtLink to="/" class="no-text-decoration">Retour à l'accueil</NuxtLink>
@@ -21,5 +25,19 @@
     justify-content: center;
     flex-direction: column;
     height: calc(100vh - 180px);
+}
+</style>
+
+<style scoped>
+.not-found-error-code {
+    text-align: center;
+    font-size: 8em;
+    margin-block-start: 0;
+    margin-block-end: 0;
+}
+.not-found-message {
+    text-align: center;
+    margin-block-start: 0;
+    margin-block-end: 0;
 }
 </style>
