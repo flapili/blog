776e2536346670fc6f180750023fc92bb5bd998e
Deveaux Benoît <contact.flapili@gmail.com>
2020-11-06 16:49:09 +0100

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page non trouvée</h1>
+    <h1 v-else>Une erreur est survenue</h1>
+    <div>
+        <NuxtLink to="/" class="no-text-decoration">Retour à l'accueil</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+  }
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: calc(100vh - 180px);
+}
+</style>
