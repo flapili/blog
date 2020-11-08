b8615e33f6946be556d44f69fa2d9ff0c4a2d035
vincentrs92 <vincent.rousseau.pro@gmail.com>
2020-11-08 18:47:01 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -14,38 +14,32 @@
     <el-row type="flex" style="margin-top: 16px">
       <el-col :md="{ span: 16, offset: 4 }">
         <el-card shadow="hover" class="transparent-card" style="text-align: justify">
-          Laboris ipsum dolor labore qui irure proident esse ut duis. Commodo
-          occaecat esse exercitation ad. Laboris qui ex id minim laborum veniam
-          eiusmod ea sunt quis reprehenderit ea. Cillum elit tempor cillum
-          labore qui aliquip consequat incididunt excepteur cupidatat ullamco
-          dolore adipisicing. Id officia duis tempor incididunt eiusmod. Sit
-          nisi laborum qui esse cillum laborum magna cillum ullamco aliqua
-          ullamco sint aute. Ex aliqua enim eiusmod enim do id eiusmod eiusmod
-          anim non.<br /><br />
-          Nulla voluptate elit nisi eiusmod elit pariatur eu enim. Elit ad qui
-          ex sunt aliqua occaecat aliqua pariatur ullamco. Aute occaecat ad amet
-          mollit aliqua Lorem deserunt velit. Excepteur irure ipsum tempor
-          exercitation est pariatur veniam qui reprehenderit.<br /><br />
-          Et est Lorem cupidatat proident labore sunt elit nostrud magna eu.
-          Reprehenderit do laborum in in. Minim aute tempor ad cillum in
-          adipisicing enim veniam sunt cupidatat reprehenderit. Ex ut sunt
-          reprehenderit mollit ex cupidatat excepteur quis tempor eu nisi qui.
-          Adipisicing velit fugiat tempor dolor qui est. Exercitation sint culpa
-          ut amet commodo.<br /><br />
-          Incididunt aliquip sit aliquip est tempor commodo irure. Veniam
-          excepteur commodo laborum reprehenderit. Dolore dolor nostrud nisi non
-          ad ut do in est eiusmod. Ipsum quis consectetur culpa laboris. Sint
-          tempor culpa do officia adipisicing nostrud minim qui. Adipisicing
-          eiusmod Lorem Lorem qui adipisicing consectetur officia velit eiusmod
-          pariatur commodo tempor laboris. Cupidatat minim ea officia cupidatat
-          cillum aliquip elit enim excepteur nisi.<br /><br />
-          Ut ullamco aute culpa in occaecat laborum velit aliqua anim nostrud
-          nulla do. Voluptate fugiat tempor in ea esse ex culpa dolore. Ea
-          ullamco enim do aute culpa adipisicing aliqua elit. Veniam ullamco
-          duis esse fugiat irure exercitation quis occaecat. Sint mollit velit
-          sunt nisi dolore dolore aute in culpa tempor. Velit elit est mollit
-          adipisicing consequat voluptate consequat fugiat ullamco magna cillum
-          ex. Voluptate eu tempor ex duis magna deserunt irure ipsum.
+              <h3>Chers développeurs,</h3>
+          <h5>Intro</h5>
+          <p>
+            Ce site a été entièrement réalisé par des passionnés d'informatique
+            qui ont pour vocation de transmettre leurs connaissances sur des
+            sujets aussi précis que variés. Cette équipe de rédacteurs et de
+            designers est à votre écoute pour toute idée sur le fonctionnement
+            du site. Le projet étant Open Source il est consultable depuis
+            Github.
+          </p>
+
+          <h5>Collaboration</h5>
+          <p>
+            Il est très simple de collaborer en intégrant vos articles sur le
+            site internet. En effet vous pouvez très simplement "Forker" le
+            projet et le modifier à votre guise. Reconnaissez qu'il est
+            tellement mieux de discuter avec les membres du discord qui sauront vous
+            guider. Un article ne se fait pas en un jour ni même en une nuit
+            c'est un travail long et fastidieux...
+          </p>
+
+
+          <div style="text-align:center;">
+            Sur ceux vous êtes les Bienvenus, l'équipe vous souhaite une bonne
+            visite!
+          </div>
         </el-card>
       </el-col>
     </el-row>
