507d0029990682fe99d0ab714ebffb1b3ee435be
Deveaux Benoît <contact.flapili@gmail.com>
2020-11-27 18:09:44 +0100

    update metas

--- pages/posts/_post.vue
+++ pages/posts/_post.vue
@@ -141,67 +141,80 @@ export default {
     if (this.article.image) {
       image_content = `https://flapili.fr/posts/${this.article.image.src}`;
     }
+    const meta = [
+      {
+        hid: "description",
+        property: "description",
+        content: this.article.description || "Blog de flapili",
+      },
+      {
+        hid: "image",
+        property: "image",
+        content: image_content,
+      },
+      // Open Graph
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: this.article.title || "flapili.fr",
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: this.article.description || "Blog de flapili",
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: image_content,
+      },
+      {
+        hid: "og:type",
+        property: "og:type",
+        content: "website",
+      },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: `flapili.fr${this.$route.path}`,
+      },
+      // Twitter Card
+      {
+        hid: "twitter:title",
+        property: "twitter:title",
+        content: this.article.title || "flapili.fr",
+      },
+      {
+        hid: "twitter:description",
+        property: "twitter:description",
+        content: this.article.description || "Blog de flapili",
+      },
+      {
+        hid: "twitter:image",
+        property: "twitter:image",
+        content: image_content,
+      },
+      {
+        hid: "twitter:card",
+        property: "twitter:card",
+        content: "summary_large_image",
+      },
+    ];
+    if (this.article.author && this.article.author.name) {
+      meta.push({
+        hid: "author",
+        property: "author",
+        content: this.article.author.name,
+      });
+      meta.push({
+        hid: "publisher",
+        property: "publisher",
+        content: this.article.author.name,
+      });
+    }
     return {
       title: this.article.title || "flapili.fr",
-      meta: [
-        {
-          hid: "description",
-          property: "description",
-          content: this.article.description || "Blog de flapili",
-        },
-        {
-          hid: "image",
-          property: "image",
-          content: image_content,
-        },
-        // Open Graph
-        {
-          hid: "og:title",
-          property: "og:title",
-          content: this.article.title || "flapili.fr",
-        },
-        {
-          hid: "og:description",
-          property: "og:description",
-          content: this.article.description || "Blog de flapili",
-        },
-        {
-          hid: "og:image",
-          property: "og:image",
-          content: image_content,
-        },
-        {
-          hid: "og:type",
-          property: "og:type",
-          content: "website",
-        },
-        {
-          hid: "og:url",
-          property: "og:url",
-          content: `flapili.fr${this.$route.path}`,
-        },
-        // Twitter Card
-        {
-          hid: "twitter:title",
-          property: "twitter:title",
-          content: this.article.title || "flapili.fr",
-        },
-        {
-          hid: "twitter:description",
-          property: "twitter:description",
-          content: this.article.description || "Blog de flapili",
-        },
-        {
-          hid: "twitter:image",
-          property: "twitter:image",
-          content: image_content,
-        },
-        {
-          hid: "twitter:card",
-          property: "twitter:card",
-          content: "summary_large_image",
-        },
-      ],
+      meta: meta,
     };
   },
 };
