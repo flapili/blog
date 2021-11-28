0707cac2692223bf15a278ae8ab505e83b70fe6f
Deveaux Benoît <contact.flapili@gmail.com>
2021-11-28 18:55:04 +0100

    discord redirect

--- plugins/menu-router-guard.js
+++
@@ -1,9 +0,0 @@
-export default function(ctx) {
-    ctx.app.router.afterEach((to, from) => {
-        if (to.path.includes("/posts")) {
-            ctx.store.commit("menu-active-item/update", "/posts")
-        } else {
-            ctx.store.commit("menu-active-item/update", to.path)
-        }
-    })
-}
--- store/menu-active-item.js
+++
@@ -1,9 +0,0 @@
-export const state = () => ({
-    activeIndex: null
-})
-
-export const mutations = {
-    update(state, activeIndex) {
-        state.activeIndex = activeIndex
-    },
-}
